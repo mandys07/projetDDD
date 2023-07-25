6ef3cd25d9516dd15b15b5089ecbca7b388a765d
mandy <mandyspiga@outlook.fr>
2023-07-25 09:16:18 +0200

    init bibliothe de class

--- StarWars/StarWars/obj/Debug/net7.0/StarWars.AssemblyInfo.cs
+++ StarWars/StarWars/obj/Debug/net7.0/StarWars.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ProjetDDD")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("StarWars")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("ProjetDDD")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ProjetDDD")]
+[assembly: System.Reflection.AssemblyProductAttribute("StarWars")]
+[assembly: System.Reflection.AssemblyTitleAttribute("StarWars")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Généré par la classe MSBuild WriteCodeFragment.
